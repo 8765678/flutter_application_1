dc153e51cdea148866b6645579536613716fe8d4
MSI\ryuzu <tosa0404810@gmail.com>
2025-05-07 16:11:45 +0900

    試しのデータ追加

---
+++ lib/data/data.dart
@@ -0,0 +1,12 @@
+class Product {
+  final String id;
+  final String name;
+  final int price;
+  final String imageUrl;
+
+  Product(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.imageUrl});
+}
